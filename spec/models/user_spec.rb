dd231d895cd2b5d151bfeb4c4f5c009d5ae5fe1a
Catalin <catalinboruga@yahoo.com>
2015-05-16 12:33:56 +0200

    Add shoulda matchers for spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,15 @@ describe User do
   it { should respond_to (:email) }
   it { should respond_to (:password) }
   it { should respond_to (:password_confirmation) } 
+  
+  it { should validate_presence_of(:email) }
+	it { should validate_uniqueness_of(:email) }
+	it { should validate_confirmation_of(:password) }
+	it { should allow_value('example@domain.com').for(:email) }
 
   it { should be_valid }
+end
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_not be_valid }
 end
